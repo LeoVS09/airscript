50797ae9bd39235785dea9c9b39ed7dd4c999e7d
LeoVS09 <leovs09@outlook.com>
2018-12-14 05:19:44 +0300

    start rewriting in typescript

--- src/index.js
+++ src/index.js
@@ -2,37 +2,48 @@ const core = require('./core')
 const {promisify} = require('util')
 const fs = require('fs')
 const path = require('path')
+const mkDir = require('make-dir')
 const config = require('../config.json')
 const airSyntaxDefinitions = require('./SyntaxDefinitions/air')
 
 const readFileAsync = promisify(fs.readFile)
 const writeFileAsync = promisify(fs.writeFile)
 
-readFileAsync(path.resolve('./' + config.source.folder + '/' + config.source.index), 'utf8')
-  .then((text) => core(text,airSyntaxDefinitions, true))
-  .then(data => data.toJS())
-  .then(text => {
-    console.log("TEXT:\n", text)
-    let buildFolder = path.resolve('./' + config.target.folder)
-    return new Promise((resolve, reject) => {
-      fs.stat(buildFolder, err => {
-        if (!err) {
-          return resolve(text)
-        }
-        fs.mkdir(buildFolder, err => {
-          if (err) {
-            console.error('Cannot create build folder ')
-            reject(err)
-          }
-          resolve(text)
+export default function build() {
+  readFileAsync(path.resolve('./' + config.source.folder + '/' + config.source.index), 'utf8')
+      .then((text) => core(text,airSyntaxDefinitions, true))
+      .then(data => data.toJS())
+      .then(text => {
+        console.log("TEXT:\n", text)
+        let buildFolder = path.resolve('./' + config.target.folder)
+        return new Promise((resolve, reject) => {
+          fs.stat(buildFolder, err => {
+            if (!err) {
+              return resolve(text)
+            }
+            fs.mkdir(buildFolder, err => {
+              if (err) {
+                console.error('Cannot create build folder ')
+                reject(err)
+              }
+              resolve(text)
+            })
+          })
         })
       })
-    })
-  })
-  .then(text => {
-    return writeFileAsync(path.resolve('./' + config.target.folder + '/' + config.target.index), text, 'utf8')
-  })
-  .catch(err => {
-    console.error(err)
-  })
+      .then(text => {
+        mkDir(path.resolve('./' + config.target.folder))
+        return writeFileAsync(path.resolve('./' + config.target.folder + '/' + config.target.index), text, 'utf8')
+      })
+      .catch(err => {
+        console.error(err)
+      })
+}
+
+export async function airToJs(text){
+  const data = await core(text, airSyntaxDefinitions)
+  return data.toJS()
+}
+
+
 
--- test/index.js
+++ test/index.js
@@ -1,3 +1,30 @@
+import {airToJs} from '../src'
 
-var air2js = require('air2js.js').default
+async function main () {
+    let air = `
+var a
+    
+var ob
+    num 1
+    str "" 
+`
+    let js = `
+let a
 
+let ob = {
+    num: 1,
+    str: ""
+}
+`
+
+    const result = await airToJs(air)
+    if(result === js){
+        console.log("Check air to js completed")
+    }else {
+        console.error("Check air to js not completed")
+        console.log('-----------------------expected\n', js)
+        console.log('-----------------------found\n', result)
+    }
+}
+
+main()
